7c8a91fe0b5746041eff5f775d71f7f356a6f4de
Masoud Banimahd <masoud@eliq.io>
2022-04-03 14:16:14 +0200

    Implement UserService and Contracts

---
+++ ServiceContracts/UserService/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceContracts
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+    }
+}
---
+++ UserService/UserService.cs
@@ -0,0 +1,21 @@
+﻿using ServiceContracts;
+using System.Threading.Tasks;
+
+namespace UserService
+{
+    public class UserService : IUserService
+    {
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            await Task.Delay(1000);
+
+            return new User
+            {
+                Id = System.Guid.NewGuid(),
+                FirstName = "Jack",
+                LastName = "Sheperd",
+                Mobile = "0730000000"
+            };
+        }
+    }
+}
